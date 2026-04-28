d9ba899919b8b31d84f67413252c5d06868747cf
Arbuz667 <emil.khalitov@narxoz.kz>
2026-04-29 01:29:58 +0500

    refactor: update CharacterSelectScreen with LocationType support Implemented basic UI text and input handling for starting the game with a specific location.

--- core/src/main/java/com/softchaos/screens/CharacterSelectScreen.java
+++ core/src/main/java/com/softchaos/screens/CharacterSelectScreen.java
@@ -1,15 +1,22 @@
 package com.softchaos.screens;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.softchaos.SoftChaosGame;
 import com.softchaos.managers.GameStateManager;
+import com.softchaos.utils.LocationType;
 
 /** 3 character cards. On click: save choice to GameStateManager → GameScreen. */
 public class CharacterSelectScreen implements Screen {
 
     private final SoftChaosGame game;
     private SpriteBatch batch;
+    private BitmapFont font;
 
     public CharacterSelectScreen(SoftChaosGame game) {
         this.game = game;
@@ -18,14 +25,32 @@ public class CharacterSelectScreen implements Screen {
     @Override
     public void show() {
         batch = new SpriteBatch();
+        font  = new BitmapFont();
+        font.setColor(Color.WHITE);
+        font.getData().setScale(1.8f);
     }
 
     @Override
     public void render(float delta) {
-        // TODO M1: render 3 character cards
-        // On card click:
-        //   GameStateManager.getInstance().selectedCharacter = index;
-        //   game.setScreen(new GameScreen(game));
+        Gdx.gl.glClearColor(0.05f, 0.05f, 0.1f, 1f);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        // TODO M4: replace with proper character card UI
+        batch.begin();
+        font.draw(batch, "SELECT LOCATION",      Gdx.graphics.getWidth() / 2f - 160, Gdx.graphics.getHeight() / 2f + 80);
+        font.draw(batch, "1 - Forest",           Gdx.graphics.getWidth() / 2f - 80,  Gdx.graphics.getHeight() / 2f + 20);
+        font.draw(batch, "2 - City",             Gdx.graphics.getWidth() / 2f - 80,  Gdx.graphics.getHeight() / 2f - 30);
+        font.draw(batch, "3 - Ocean",            Gdx.graphics.getWidth() / 2f - 80,  Gdx.graphics.getHeight() / 2f - 80);
+        batch.end();
+
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_1)) startGame(LocationType.FOREST);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_2)) startGame(LocationType.CITY);
+        if (Gdx.input.isKeyJustPressed(Input.Keys.NUM_3)) startGame(LocationType.OCEAN);
+    }
+
+    private void startGame(LocationType location) {
+        GameStateManager.getInstance().currentLocation = location;
+        game.setScreen(new GameScreen(game));
     }
 
     @Override public void resize(int width, int height) {}
@@ -40,5 +65,6 @@ public class CharacterSelectScreen implements Screen {
     @Override
     public void dispose() {
         if (batch != null) { batch.dispose(); batch = null; }
+        if (font  != null) { font.dispose();  font  = null; }
     }
 }
